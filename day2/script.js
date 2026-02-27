44a716082d24b2c301c0bdf7fd2aea3f4aa1ec41
gunasree2006 <pullareddygarigunasree@gmail.com>
2026-02-27 14:43:01 +0530

    Add files via upload

---
+++ day2/script.js
@@ -0,0 +1,40 @@
+/*let a=10;
+{
+    let a=20;
+}
+console.log(a);
+*/
+
+
+/*
+let obj1={
+    name:'abc',
+    age:21
+}
+let obj2=obj1
+obj2.name='xyz';
+
+
+console.log(obj1.name)
+if("")
+{
+    console.log('true')
+}
+else{
+    console.log('false')
+}
+    */
+   console.log(5+'5') //55
+   console.log(5-'5') //0
+   console.log(5=='5') //true
+   console.log(5==='5') //false
+
+   //== -> is used to check the value whereas === ->is used to check the datatype
+
+   //undefined means variable is declared but not initialised and if any function doesnot return any value means it is considered as undefined
+   //null is assigned by developer
+
+   //in java array stores same datatypes whereas in javascript it stores different datatypes
+
+   let arr=[1,'new','2',3]
+   console.log(arr)
